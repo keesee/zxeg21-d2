4df562407016705cfb7df7c26288faa1254a3e78
Jimmy Keesee <jimmy@keesee.net>
2019-09-28 21:04:48 +0800

    end of class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 var n1 = require('./number.js')
 var sqFunction = require('./sq')
+var stats = require('simple-statistics')
+var numList = require('./numlist')
 
 
-var combine = sqFunction(n1)
-console.log(combine)
+
+// var combine = sqFunction(n1)
+// console.log(combine)
+
+console.log(stats.min(numList))
